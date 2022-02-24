535afd187eaa8600a942b23c39eeec30572619fb
malte <malteut@gmail.com>
2022-02-24 13:59:54 +0100

    Added rsa vrf

--- rsa-fdh-vrf/src/rsa-fdh-vrf.rs
+++ rsa-fdh-vrf/src/rsa-fdh-vrf.rs
@@ -1,18 +1,38 @@
 use hacspec_lib::*;
 use rsa::*;
+use hacspec_sha256::*;
 
 // TODO check if we should include mgf_salt
-pub fn prove(n: usize, e: usize, alpha: &ByteSeq) -> ByteSeq {
-    unimplemented!()
+pub fn prove(sk: SK, alpha: &ByteSeq) -> ByteSeq {
+    let mgf_domain_separator = ByteSeq::from_hex("01");
+    // Add suite string when suite stuff is done, MGF salt stuff
+    // let mgf_salt = ByteSeq::from_hex("01")
+    // Do proper k value
+    let em = mgf1(&mgf_domain_separator.concat(alpha), 128 - 1);
+    let m = os2ip(&em);
+    let s = rsasp1(sk, m);
+    i2osp(s, 128)
 }
 
-pub fn proof_to_hash(pi: &ByteSeq) -> ByteSeq {
-    unimplemented!()
+pub fn proof_to_hash(pi_string: &ByteSeq) -> ByteSeq {
+    let proof_to_hash_domain_separator = ByteSeq::from_hex("02");
+    let sha_digest = sha256(&proof_to_hash_domain_separator.concat(pi_string));
+    // TODO this is stupid
+    let empty = ByteSeq::new(0);
+    empty.concat(&sha_digest)
 }
 
 // TODO check if we should include mgf_salt
-pub fn verify(n: usize, d: usize, alpha: &ByteSeq, pi: &ByteSeq) -> bool {
-    unimplemented!()
+pub fn verify(pk: PK, alpha: &ByteSeq, pi_string: &ByteSeq) -> bool {
+    let s = os2ip(pi_string);
+    // Check if signature representative is out of range
+    let m = rsavp1(pk, s);
+    let mgf_domain_separator = ByteSeq::from_hex("01");
+    // TODO add all the correct string things
+    let em = mgf1(&mgf_domain_separator.concat(alpha), 128 - 1);
+    let new_m = os2ip(&em);
+    // TODO do proper return type as in spec
+    m == new_m
 }
 
 // TODO check what we should do for the ciphersuites (different hash functions)
