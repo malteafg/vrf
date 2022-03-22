ba3adef30b56e358777f85708a65d4b434015ad0
malte <malteut@gmail.com>
2022-03-22 13:14:40 +0100

    ed25519-hash complete

--- ed25519-hash/src/ed25519-hash.rs
+++ ed25519-hash/src/ed25519-hash.rs
@@ -12,7 +12,6 @@ const Z: u128 = 2;
 
 array!(ArrEd25519FieldElement, 4, U64);
 
-// TODO should these be in little endian?
 // (p - 1) / 2
 const P_1_2: ArrEd25519FieldElement = ArrEd25519FieldElement(secret_array!(
     U64,
@@ -26,7 +25,9 @@ const P_1_2: ArrEd25519FieldElement = ArrEd25519FieldElement(secret_array!(
 
 // taken from bls12-381-hash.rs
 // https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#section-5.4.1
-pub fn expand_message_xmd(msg: &ByteSeq, dst: &ByteSeq, len_in_bytes: usize) -> ByteSeq {
+pub fn expand_message_xmd(
+    msg: &ByteSeq, dst: &ByteSeq, len_in_bytes: usize
+) -> ByteSeq {
     let ell = (len_in_bytes + B_IN_BYTES - 1) / B_IN_BYTES; // ceil(len_in_bytes / b_in_bytes)
                                                             // must be that ell <= 255
     let dst_prime = dst.push(&U8_from_usize(dst.len())); // DST || I2OSP(len(DST), 1)
@@ -34,17 +35,22 @@ pub fn expand_message_xmd(msg: &ByteSeq, dst: &ByteSeq, len_in_bytes: usize) -> 
     let mut l_i_b_str = ByteSeq::new(2);
     l_i_b_str[0] = U8_from_usize(len_in_bytes / 256);
     l_i_b_str[1] = U8_from_usize(len_in_bytes); // I2OSP(len_in_bytes, 2)
+
     let msg_prime = z_pad
         .concat(msg)
         .concat(&l_i_b_str)
         .concat(&ByteSeq::new(1))
         .concat(&dst_prime); // Z_pad || msg || l_i_b_str || 0 || dst_prime
+    
     let b_0 = ByteSeq::from_seq(&hash(&msg_prime)); // H(msg_prime)
-    let mut b_i = ByteSeq::from_seq(&hash(&b_0.push(&U8(1u8)).concat(&dst_prime))); // H(b_0 || 1 || dst_prime)
+    let mut b_i = ByteSeq::from_seq(&hash(&b_0.push(&U8(1u8))
+        .concat(&dst_prime))); // H(b_0 || 1 || dst_prime)
     let mut uniform_bytes = ByteSeq::from_seq(&b_i);
+
     for i in 2..(ell + 1) {
         let t = ByteSeq::from_seq(&b_0);
-        b_i = ByteSeq::from_seq(&hash(&(t ^ b_i).push(&U8_from_usize(i)).concat(&dst_prime))); //H((b_0 ^ b_(i-1)) || 1 || dst_prime)
+        b_i = ByteSeq::from_seq(&hash(&(t ^ b_i).push(&U8_from_usize(i))
+            .concat(&dst_prime))); //H((b_0 ^ b_(i-1)) || 1 || dst_prime)
         uniform_bytes = uniform_bytes.concat(&b_i);
     }
     uniform_bytes.truncate(len_in_bytes)
@@ -52,18 +58,18 @@ pub fn expand_message_xmd(msg: &ByteSeq, dst: &ByteSeq, len_in_bytes: usize) -> 
 
 // adapted from bls12-381-hash.rs
 // https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#section-5.3
-pub fn ed_hash_to_field(msg: &ByteSeq, dst: &ByteSeq, count: usize) -> Seq<Ed25519FieldElement> {
+pub fn ed_hash_to_field(
+    msg: &ByteSeq, dst: &ByteSeq, count: usize
+) -> Seq<Ed25519FieldElement> {
     let len_in_bytes = count * L; // count * m * L
     let uniform_bytes = expand_message_xmd(msg, dst, len_in_bytes);
     let mut output = Seq::<Ed25519FieldElement>::new(count);
+
     for i in 0..count {
         // m = 1, so no loop
         let elm_offset = L * i; // L * (j + i * m)
         let tv = uniform_bytes.slice(elm_offset, L); //substr(uniform_bytes, elm_offset, L)
-        let u_i =
-            // TODO why does the bls hash, use a bigger canvas?
-            Ed25519FieldElement::from_byte_seq_be(&tv); // OS2IP(tv) mod p
-            // Ed25519FieldElement::from_byte_seq_be(&FpHash::from_byte_seq_be(&tv).to_byte_seq_be().slice(16, 48)); // OS2IP(tv) mod p
+        let u_i = Ed25519FieldElement::from_byte_seq_be(&tv); // OS2IP(tv) mod p
         output[i] = u_i;
     }
     output
@@ -76,9 +82,8 @@ fn ed_is_square(x: Ed25519FieldElement) -> bool {
     tv == Ed25519FieldElement::ZERO() || tv == Ed25519FieldElement::ONE()
 }
 
-// Returns true if x is odd and false otherwise?
-// TODO what the fuck
-fn ed_sgn0(x: Ed25519FieldElement) -> bool {
+// https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#section-4.1-5
+fn sgn0_m_eq_1(x: Ed25519FieldElement) -> bool {
     x % Ed25519FieldElement::TWO() == Ed25519FieldElement::ONE()
 }
 
@@ -109,13 +114,17 @@ fn map_to_curve_elligator2(u: Ed25519FieldElement) -> Point {
     let mut y = zero;
     if ed_is_square(gx1) {
         x = x1;
-        // TODO what about the sgn0 function? what does it all mean, the numbers
         // TODO what to do with unwrap?
         y = sqrt(gx1).unwrap();
+        if sgn0_m_eq_1(y) {
+            y = zero - sqrt(gx1).unwrap();
+        }
     } else {
-        // sgn0 weirdness still
         x = x2;
         y = sqrt(gx2).unwrap();
+        if !sgn0_m_eq_1(y) {
+            y = zero - sqrt(gx1).unwrap();
+        }
     }
     let s = x * k;
     let t = y * k;
@@ -125,7 +134,7 @@ fn map_to_curve_elligator2(u: Ed25519FieldElement) -> Point {
     X25519FieldElement::from_byte_seq_le(t.to_byte_seq_le()))
 }
 
-// https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#name-mappings-for-montgomery-cur
+// https://www.ietf.org/archive/id/draft-irtf-cfrg-hash-to-curve-13.html#appendix-D.1-13
 fn monty_to_edw(p: Point) -> EdPoint {
     let (s,t) = p;
     let one = X25519FieldElement::ONE();
